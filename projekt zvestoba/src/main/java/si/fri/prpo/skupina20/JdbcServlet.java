76696a7d9433ea586f915dd738818e34a04501ce
zanjonke <zj0527@student.uni-lj.si>
2017-10-18 21:21:06 +0200

    uredu sintaksa JDBCServlet

--- projekt zvestoba/src/main/java/si/fri/prpo/skupina20/JdbcServlet.java
+++ projekt zvestoba/src/main/java/si/fri/prpo/skupina20/JdbcServlet.java
@@ -1,20 +1,22 @@
 package si.fri.prpo.skupina20;
-import javax.servlet.http.HttpServlet;
-import javax.annotitaion.webservlet;
+        import javax.servlet.ServletException;
+        import javax.servlet.http.HttpServlet;
+        import javax.servlet.annotation.WebServlet;
+        import javax.servlet.http.*;
+        import java.io.*;
 
 @WebServlet("servlet")
-
 public class JdbcServlet extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         // implementacija
-        printWriter writer = resp.getWrtier();
+        PrintWriter writer = resp.getWriter();
 
         writer.append("Hello World\n");
 
-        String serviceName = ConfigurationUtil.getInstance(),get("kumuluzee.name").orElse(other:"VREDNOSTI NI");
-        writer.append(serviceName);
+        //String serviceName = ConfigurationUtil.getInstance().get("kumuluzee.name").orElse(other:"VREDNOSTI NI");
+       // writer.append(serviceName);
 
     }
 }
