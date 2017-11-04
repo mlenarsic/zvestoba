ed353b172d716949676527583b058bd5857fbccb
zanjonke <zj0527@student.uni-lj.si>
2017-11-04 14:05:30 +0100

    uporaba ds in priprava pu in del testiranja

---
+++ projekt_zvestoba/api/src/main/java/JPAServlet.java
@@ -0,0 +1,28 @@
+import javax.inject.Inject;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.util.List;
+
+@WebServlet("/servlet")
+public class JPAServlet extends HttpServlet {
+
+    @Inject
+    private UporabnikiZrno uporabnikiZrno;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws SecurityException, IOException {
+
+        PrintWriter writer = resp.getWriter();
+        List<Uporabnik> uporabniki = uporabnikiZrno.getUporabniki();
+        for(int i = 0; i < uporabniki.size(); i++){
+            Uporabnik u = uporabniki.get(i);
+            writer.append(u.getIme() + " " + u.getPriimek() + " " + u.getUporabnisko_ime() + " " + u.getId());
+        }
+
+    }
+}
