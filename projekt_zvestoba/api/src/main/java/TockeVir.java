61740bd3625528a2587a58697b6c804a1066b3f4
Unknown <zj0527@student.uni-lj.si>
2017-12-18 12:37:58 +0100

    koncana 7. naloga

--- projekt_zvestoba/api/src/main/java/TockeVir.java
+++ projekt_zvestoba/api/src/main/java/TockeVir.java
@@ -1,10 +1,14 @@
 
 import com.kumuluz.ee.rest.beans.QueryParameters;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.ws.rs.*;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.*;
+import java.util.LinkedList;
 import java.util.List;
 
 @Path("tocke")
@@ -13,18 +17,44 @@ import java.util.List;
 @ApplicationScoped
 public class TockeVir {
 
+    private Client httpClient;
+
     @Inject
     private TockeZrno tBean;
 
     @Context
     protected UriInfo uriInfo;
 
+    @PostConstruct
+    private void init() {
+        httpClient = ClientBuilder.newClient();
+        String baseUrl = "http://192.168.99.100:8081/v1";
+    }
+
+    public List<Tocke_ponudnik> vrniTockeSPonudniki(List<Tocke> tocke){
+        List<Tocke_ponudnik> tp = new LinkedList<>();
+        for(int i = 0; i < tocke.size(); i++){
+            Tocke t = tocke.get(i);
+            Ponudnik p = httpClient
+                    .target("http://192.168.99.100:8081/v1/ponudniki/" + t.getPonudnik_id())
+                    .request()
+                    .get(new GenericType<Ponudnik>(){});
+            Tocke_ponudnik TP = new Tocke_ponudnik();
+            TP.setId_kartice(t.getId_kartice());
+            TP.setZbrane_tocke(t.getZbrane_tocke());
+            TP.setUporabnik(t.getUporabnik());
+            TP.setPonudnik(p);
+            tp.add(TP);
+        }
+        return tp;
+    }
+
     @GET
     public Response vrniTocke(){
-
         QueryParameters query = QueryParameters.query(uriInfo.getRequestUri().getQuery()).build();
         List<Tocke> tocke = tBean.pridobiTocke(query);
-        return Response.status(Response.Status.OK).entity(tocke).build();
+        List<Tocke_ponudnik> tp = vrniTockeSPonudniki(tocke);
+        return Response.status(Response.Status.OK).entity(tp).build();
 
     }
 
---
+++ projekt_zvestoba/api/src/main/java/Tocke_ponudnik.java
@@ -0,0 +1,38 @@
+public class Tocke_ponudnik {
+
+    private Integer id_kartice;
+
+    private Integer zbrane_tocke;
+
+    private Uporabnik uporabnik;
+
+    private Ponudnik ponudnik;
+    public Integer getId_kartice(){return id_kartice;}
+
+    public void setId_kartice(Integer id_kartice) {
+        this.id_kartice = id_kartice;
+    }
+
+    public Ponudnik getPonudnik() { return ponudnik; }
+
+    public void setPonudnik(Ponudnik ponudnik) {
+        this.ponudnik = ponudnik;
+    }
+
+    public Integer getZbrane_tocke() {
+        return zbrane_tocke;
+    }
+
+    public void setZbrane_tocke(Integer zbrane_tocke) {
+        this.zbrane_tocke = zbrane_tocke;
+    }
+
+    public Uporabnik getUporabnik() {
+        return uporabnik;
+    }
+
+    public void setUporabnik(Uporabnik uporabnik) {
+        this.uporabnik = uporabnik;
+    }
+
+}
